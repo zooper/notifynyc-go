e997d3a408a97fd084990dec5aa9741cf74303cb
Tomas Jonsson <jonsson@spotify.com>
2023-04-14 08:01:07 -0400

    Inital commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module notifynyc
+
+go 1.19
+
+require github.com/matrix-org/gomatrix v0.0.0-20220926102614-ceba4d9f7530
---
+++ main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"encoding/xml"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"strings"
+)
+
+type RSS struct {
+    XMLName xml.Name `xml:"rss"`
+    Channel Channel  `xml:"channel"`
+}
+
+type Channel struct {
+    Title       string `xml:"title"`
+    Link        string `xml:"link"`
+    Description string `xml:"description"`
+    Items       []Item   `xml:"item"`
+}
+
+type Item struct {
+    Title       string `xml:"title"`
+    Description string `xml:"description"`
+    PubDate     string `xml:"pubDate"`
+}
+
+func main() {
+	f, err := os.OpenFile("log.txt", os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+    resp, err := http.Get("https://a858-nycnotify.nyc.gov/RSS/NotifyNYC?lang=en")
+    if err != nil {
+        panic(err)
+    }
+    defer resp.Body.Close()
+
+    body, err := ioutil.ReadAll(resp.Body)
+    if err != nil {
+        panic(err)
+    }
+
+    var rss RSS
+    if err := xml.Unmarshal(body, &rss); err != nil {
+        panic(err)
+    }
+	for _, item := range rss.Channel.Items {
+
+		file, err := os.ReadFile("log.txt")
+		if err != nil {
+			fmt.Println(err)
+		}
+		if strings.Contains(string(file), item.PubDate) {
+			break	
+
+		} else {
+			// Write the new entries to file
+			f.WriteString(item.PubDate + "\n")
+			matrix(item.PubDate, item.Description, item.Title)
+		}
+	}
+	f.Close()
+}
+
